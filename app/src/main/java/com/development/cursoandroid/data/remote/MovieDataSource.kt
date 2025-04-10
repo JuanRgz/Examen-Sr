cdd778d43e83a9b71ad6960055c22e962ed70d86
Juan Rodriguez <juan.fco946@gmail.com>
2025-04-10 15:57:36 -0600

    Finalizando data source

--- app/src/main/java/com/development/cursoandroid/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/development/cursoandroid/data/remote/MovieDataSource.kt
@@ -1,15 +1,13 @@
 package com.development.cursoandroid.data.remote
 
+import com.development.cursoandroid.application.ApiConstants
 import com.development.cursoandroid.data.model.MovieList
+import com.development.cursoandroid.repository.WebService
 
-class MovieDataSource {
-    fun getUpcomingMovies(): MovieList{
-        return MovieList()
-    }
-    fun getTopRatedMovies(): MovieList{
-        return MovieList()
-    }
-    fun getPopularMovies(): MovieList{
-        return MovieList()
-    }
+class MovieDataSource(private val webService: WebService) {
+    suspend fun getUpcomingMovies(): MovieList = webService.getUpcomingMovies(ApiConstants.API_KEY)
+
+    suspend fun getTopRatedMovies(): MovieList = webService.getTopRatedMovies(ApiConstants.API_KEY)
+
+    suspend fun getPopularMovies(): MovieList = webService.getPopularMovies(ApiConstants.API_KEY)
 }
--- app/src/main/java/com/development/cursoandroid/repository/WebService.kt
+++ app/src/main/java/com/development/cursoandroid/repository/WebService.kt
@@ -10,13 +10,13 @@ import retrofit2.http.Query
 
 interface WebService {
     @GET("upcoming")
-    fun getUpcomingMovies(@Query("api_key") apiKey: String): MovieList
+    suspend fun getUpcomingMovies(@Query("api_key") apiKey: String): MovieList
 
     @GET("top_rated")
-    fun getTopRatedMovies(@Query("api_key") apiKey: String): MovieList
+    suspend fun getTopRatedMovies(@Query("api_key") apiKey: String): MovieList
 
     @GET("popular")
-    fun getPopularMovies(@Query("api_key") apiKey: String): MovieList
+    suspend fun getPopularMovies(@Query("api_key") apiKey: String): MovieList
 }
 
 object RetrofitClient{
